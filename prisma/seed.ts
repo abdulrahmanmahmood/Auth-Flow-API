49201ee68e79b0f70d60a16d1aacf665eab48df2
abdulrahmanmahmood <mailto:abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2025-11-10 11:34:09 +0200

    feat(prisma): initialize Prisma schema and models for user authentication and todos
    - Added Prisma schema with models: Users, verificationToken, RefreshToken, OAuthAccount, PasswordResetToken, and Todo.
    - Defined enums for AuthProvider and Priority.
    - Generated necessary files for Prisma client and WASM engine.
    - Implemented PrismaModule and PrismaService for NestJS integration.
    - Established database connection handling in PrismaService.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from './generated/prisma/client.js';
 import { faker } from '@faker-js/faker';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UsersModel } from 'prisma/generated/prisma/models';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): Promise<UsersModel[]> {
+  getHello() {
     return this.appService.getUserList();
   }
 }
