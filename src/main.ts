1ed6a5d2093290a26acf574f4670febcdb24bb98
abdulrahmanmahmood <mailto:abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2025-11-10 15:30:02 +0200

    feat(auth): implement authentication module with controller, service, and DTOs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const apiPrefix = 'api';
+  app.setGlobalPrefix(apiPrefix); // should be above the listening or will not work
+
   await app.listen(process.env.PORT ?? 5001);
 }
 bootstrap();
