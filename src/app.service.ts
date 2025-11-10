4d3d98ed9d238d2dcac4ba15bad897912876321d
abdulrahmanmahmood <mailto:abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2025-11-10 02:45:01 +0200

    feat: update AppController and AppService to return user list from database

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from 'prisma/generated/prisma/client';
+import { UsersModel } from 'prisma/generated/prisma/models';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaClient) {}
+  getUserList(): Promise<UsersModel[]> {
+    return this.prisma.users.findMany();
   }
 }
