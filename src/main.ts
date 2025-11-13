6b986ae8a1e3199d2ac88ec84e91a3f0acc8f9ab
abdulrahmanmahmood <mailto:abdulrahmanabdultawab189_sd@fcis.bsu.edu.eg>
2025-11-13 21:45:17 +0200

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -17,8 +18,17 @@ async function bootstrap() {
   );
 
   app.setBaseViewsDir('src/views');
-
   app.setViewEngine('hbs');
+
+  // Api Doc
+  const config = new DocumentBuilder()
+    .setTitle('Auth flow')
+    .setDescription('Authentication flow with all features')
+    .setVersion('1.0')
+    .addTag('Auth')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 5001, '0.0.0.0');
 }
